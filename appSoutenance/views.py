9021d788f02ef2f0f873674644ad97820ecb5707
Cherry5990 <maelyss.fronton@etu.univ-orleans.fr>
2025-11-03 12:26:20 +0100

    Mise en place des pages : liens fonctionnels, css et images accessibles

--- appSoutenance/views.py
+++ appSoutenance/views.py
@@ -1,8 +1,22 @@
 from .app import app
+from flask import render_template, request, url_for , redirect
 
 @app.route('/')
+@app.route('/index/')
 def index():
-    return "Hello world !"
+    return render_template("index.html")
+
+@app.route('/enseignant/')
+def accueil_enseignant():
+    return render_template("prof/accueil_enseignant.html")
+
+@app.route('/etudiant/')
+def accueil_etudiant():
+    return render_template("etudiant/accueil_etu.html")
+
+@app.route('/admin/')
+def accueil_admin():
+    return render_template("admin/accueil_admin.html")
 
 if __name__== "__main__":
     app.run()
