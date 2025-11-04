bda2e403ed0622a9ddbfcc03be487e9ef652da02
Erika <o22401110@campus.univ-orleans.fr>
2025-11-04 09:02:12 +0100

    modification du nom des attributs dans les init et les repr

--- appSoutenance/models.py
+++ appSoutenance/models.py
@@ -10,16 +10,16 @@ class Entreprise (db.Model):
     email_entreprise = db.Column(db.String(200))
 
     def __init__(self, nom_entreprise, secteur, adresse, type, tel_entreprise, email_entreprise):
-        self.Nom_entreprise = nom_entreprise
-        self.Secteur = secteur
-        self.Adresse = adresse
-        self.Type = type
-        self.Tel_entreprise = tel_entreprise
-        self.Email_entreprise = email_entreprise
+        self.nom_entreprise = nom_entreprise
+        self.secteur = secteur
+        self.adresse = adresse
+        self.typeE = type
+        self.tel_entreprise = tel_entreprise
+        self.email_entreprise = email_entreprise
     
     def __repr__(self):
-        return f"<Entreprise {self.Nom_entreprise} de type {self.Type} dans le secteur {self.Secteur} situé à {self.Adresse}. \nTel: {self.Tel_entreprise} \nEmail: {self.Email_entreprise}>"
-    
+        return f"<Entreprise {self.nom_entreprise} de type {self.typeE} dans le secteur {self.secteur} situé à {self.adresse}. \nTel: {self.tel_entreprise} \nEmail: {self.email_entreprise}>"
+
 class Demarche(db.Model):
     id_demarche = db.Column(db.Integer, primary_key = True)
     source = db.Column(db.String(300), nullable = False)
@@ -35,19 +35,19 @@ class Demarche(db.Model):
     entreprise = db.relationship("Entreprise", backref = db.backref("demarches", lazy = "dynamic"))
 
     def __init__(self, id_demarche, source, type, situation, date_envoi, date_relance, resultat, raison_refus, cv, lettre):
-        self.Id_demarche = id_demarche
-        self.Source = source
-        self.Type = type
-        self.Situation = situation
-        self.Date_envoi = date_envoi
-        self.Date_relance = date_relance
-        self.Resultat = resultat
-        self.Raison_refus = raison_refus
-        self.Cv = cv
-        self.Lettre_motiv = lettre
-    
+        self.id_demarche = id_demarche
+        self.source = source
+        self.typeD = type
+        self.situation = situation
+        self.date_envoi = date_envoi
+        self.date_relance = date_relance
+        self.resultat = resultat
+        self.raison_refus = raison_refus
+        self.cv = cv
+        self.lettre_motiv = lettre
+
     def __repr__(self):
-        return f"<Demarche {self.Id_demarche} de type {self.Type} envoyée le {self.Date_envoi} pour l'entreprise {self.entreprise.Nom_entreprise}. Situation actuelle: {self.Situation}>"
+        return f"<Demarche {self.id_demarche} de type {self.typeD} envoyée le {self.date_envoi} pour l'entreprise {self.entreprise.nom_entreprise}. Situation actuelle: {self.situation}>"
 
 
 class Stage(db.Model):
@@ -63,13 +63,13 @@ class Stage(db.Model):
     demarche = db.relationship("Demarche", backref = db.backref("stages", lazy = "dynamic"))
 
     def __init__(self, type, duree, date_debut, sujet, description, competence, revenu):
-        self.Type = type
-        self.Duree = duree
-        self.Date_debut = date_debut
-        self.Sujet = sujet
-        self.Description = description
-        self.Competence = competence
-        self.Revenu = revenu
+        self.typeS = type
+        self.duree = duree
+        self.date_debut = date_debut
+        self.sujet = sujet
+        self.description = description
+        self.competence = competence
+        self.revenu = revenu
 
     def __repr__(self):
-        return f"<Stage {self.Sujet} de type {self.Type} débutant le {self.Date_debut} pour une durée de {self.Duree} semaines>"
+        return f"<Stage {self.sujet} de type {self.typeS} débutant le {self.date_debut} pour une durée de {self.duree} semaines>"
