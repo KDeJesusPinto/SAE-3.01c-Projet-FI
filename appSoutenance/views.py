feae56b053fb50fc6ea30b69806fa4d24c6d1a7c
PernelleBlandin <pernelleblandin@gmail.com>
2025-11-05 11:14:23 +0100

    Modification {{title}} dans base.html et ajout de routes et mise en forme views.py

--- appSoutenance/views.py
+++ appSoutenance/views.py
@@ -2,21 +2,34 @@ from .app import app
 from flask import render_template, request, url_for , redirect
 
 @app.route('/')
-@app.route('/index/')
+@app.route('/connexion/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Soutenance - Connexion")
 
+#Pour page enseignants
 @app.route('/enseignant/')
 def accueil_enseignant():
-    return render_template("prof/accueil_enseignant.html")
+    return render_template("prof/accueil_enseignant.html", title="Accueil")
 
+@app.route('/enseignant/liste+etu/')
+def liste_etu_enseignant():
+    return render_template("prof/lst_etudiants_enseignant.html", title="Liste des étudiants")
+
+
+#Pour page étudiants
 @app.route('/etudiant/')
 def accueil_etudiant():
-    return render_template("etudiant/accueil_etu.html")
+    return render_template("etudiant/accueil_etu.html", title="Accueil")
 
+
+
+#Pour page administrateur
 @app.route('/admin/')
 def accueil_admin():
-    return render_template("admin/accueil_admin.html")
+    return render_template("admin/accueil_admin.html", title="Accueil")
+
+
+
 
 if __name__== "__main__":
     app.run()
