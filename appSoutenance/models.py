8b035537389a361b544da4556f42075015b2840c
Erika <o22401110@campus.univ-orleans.fr>
2025-11-05 10:48:52 +0100

    formatage du code à l'aide de yapf, ajout du fichier .google à la racine du projet

--- appSoutenance/models.py
+++ appSoutenance/models.py
@@ -1,17 +1,26 @@
 from .app import db
 
+
 class Entreprise(db.Model):
-    id_entreprise = db.Column(db.Integer, primary_key = True)
-    nom_entreprise = db.Column(db.String(200), nullable = False)
-    secteur = db.Column(db.String(200), nullable = False)
-    ville = db.Column(db.String(100), nullable = False)
-    adresse = db.Column(db.String(200), nullable = False)
-    code_postal = db.Column(db.String(5), nullable = False)
-    typeE = db.Column(db.String(100), nullable = False)
+    id_entreprise = db.Column(db.Integer, primary_key=True)
+    nom_entreprise = db.Column(db.String(200), nullable=False)
+    secteur = db.Column(db.String(200), nullable=False)
+    ville = db.Column(db.String(100), nullable=False)
+    adresse = db.Column(db.String(200), nullable=False)
+    code_postal = db.Column(db.String(5), nullable=False)
+    typeE = db.Column(db.String(100), nullable=False)
     tel_entreprise = db.Column(db.String(15))
     email_entreprise = db.Column(db.String(200))
 
-    def __init__(self, nom_entreprise, secteur, ville, adresse, code_postal, typeE, tel_entreprise = None, email_entreprise = None):
+    def __init__(self,
+                 nom_entreprise,
+                 secteur,
+                 ville,
+                 adresse,
+                 code_postal,
+                 typeE,
+                 tel_entreprise=None,
+                 email_entreprise=None):
         self.nom_entreprise = nom_entreprise
         self.secteur = secteur
         self.adresse = adresse
@@ -20,17 +29,17 @@ class Entreprise(db.Model):
         self.typeE = typeE
         self.tel_entreprise = tel_entreprise
         self.email_entreprise = email_entreprise
-    
+
     def __repr__(self):
         return f"<Entreprise {self.nom_entreprise} de type {self.typeE} dans le secteur {self.secteur} situé à {self.ville} {self.code_postal} {self.adresse}. \nTel: {self.tel_entreprise} \nEmail: {self.email_entreprise}>"
 
 
 class Demarche(db.Model):
-    id_demarche = db.Column(db.Integer, primary_key = True)
-    source = db.Column(db.String(300), nullable = False)
-    typeD = db.Column(db.String(100), nullable = False)
-    situation = db.Column(db.String(300), nullable = False)
-    date_envoi = db.Column(db.Date, nullable = False)
+    id_demarche = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.String(300), nullable=False)
+    typeD = db.Column(db.String(100), nullable=False)
+    situation = db.Column(db.String(300), nullable=False)
+    date_envoi = db.Column(db.Date, nullable=False)
     date_relance = db.Column(db.Date)
     resultat = db.Column(db.String(300))
     raison_refus = db.Column(db.String(300))
@@ -38,14 +47,31 @@ class Demarche(db.Model):
     lettre_motiv = db.Column(db.String(200))
 
     # 1,1
-    id_entreprise = db.Column(db.Integer, db.ForeignKey("entreprise.id_entreprise"), nullable = False)
-    entreprise = db.relationship("Entreprise", backref = db.backref("demarches", lazy = "joined"))
+    id_entreprise = db.Column(db.Integer,
+                              db.ForeignKey("entreprise.id_entreprise"),
+                              nullable=False)
+    entreprise = db.relationship("Entreprise",
+                                 backref=db.backref("demarches", lazy="joined"))
 
     # 1.1
-    id_etudiant = db.Column(db.Integer, db.ForeignKey("etudiant.id_etudiant"), nullable = False)
-    etudiant = db.relationship("Etudiant", backref = db.backref("demarches", lazy = "joined"))
+    id_etudiant = db.Column(db.Integer,
+                            db.ForeignKey("etudiant.id_etudiant"),
+                            nullable=False)
+    etudiant = db.relationship("Etudiant",
+                               backref=db.backref("demarches", lazy="joined"))
 
-    def __init__(self, source, typeD, situation, date_envoi, id_entreprise, id_etudiant, date_relance = None, resultat = None, raison_refus = None, cv = None, lettre_motiv = None):
+    def __init__(self,
+                 source,
+                 typeD,
+                 situation,
+                 date_envoi,
+                 id_entreprise,
+                 id_etudiant,
+                 date_relance=None,
+                 resultat=None,
+                 raison_refus=None,
+                 cv=None,
+                 lettre_motiv=None):
         self.source = source
         self.typeD = typeD
         self.situation = situation
@@ -63,26 +89,46 @@ class Demarche(db.Model):
 
 
 class Stage(db.Model):
-    id_stage = db.Column(db.Integer, primary_key = True)
-    typeS = db.Column(db.String(100), nullable = False)
-    date_debut = db.Column(db.Date, nullable = False)
-    date_fin = db.Column(db.Date, nullable = False)
+    id_stage = db.Column(db.Integer, primary_key=True)
+    typeS = db.Column(db.String(100), nullable=False)
+    date_debut = db.Column(db.Date, nullable=False)
+    date_fin = db.Column(db.Date, nullable=False)
     titre_stage = db.Column(db.String(100))
     theme_stage = db.Column(db.String(100))
 
     # 1,1
-    id_demarche = db.Column(db.Integer, db.ForeignKey("demarche.id_demarche"), unique = True, nullable = False)
-    demarche = db.relationship("Demarche", backref = db.backref("stage", lazy = "joined", uselist = False))
+    id_demarche = db.Column(db.Integer,
+                            db.ForeignKey("demarche.id_demarche"),
+                            unique=True,
+                            nullable=False)
+    demarche = db.relationship("Demarche",
+                               backref=db.backref("stage",
+                                                  lazy="joined",
+                                                  uselist=False))
 
     # 0,1
-    id_maitre = db.Column(db.Integer, db.ForeignKey("maitre_stage.id_maitre"), unique = True, nullable = True)
-    maitre_stage = db.relationship("MaitreStage", backref = db.backref("stage", lazy = "select", uselist = False))
+    id_maitre = db.Column(db.Integer,
+                          db.ForeignKey("maitre_stage.id_maitre"),
+                          unique=True,
+                          nullable=True)
+    maitre_stage = db.relationship("MaitreStage",
+                                   backref=db.backref("stage",
+                                                      lazy="select",
+                                                      uselist=False))
 
     # 0,1
     # id_soutenance = db.Column(db.Integer, db.ForeignKey("soutenance.id_soutenance"), unique = True, nullable = True)
     # soutenance = db.relationship("Soutenance", backref = db.backref("stage", lazy = "select", uselist = False))
 
-    def __init__(self, typeS, date_debut, date_fin, titre_stage, theme_stage, id_demarche, id_maitre = None, id_soutenance = None):
+    def __init__(self,
+                 typeS,
+                 date_debut,
+                 date_fin,
+                 titre_stage,
+                 theme_stage,
+                 id_demarche,
+                 id_maitre=None,
+                 id_soutenance=None):
         self.typeS = typeS
         self.date_debut = date_debut
         self.date_fin = date_fin
@@ -97,18 +143,29 @@ class Stage(db.Model):
 
 
 class MaitreStage(db.Model):
-    id_maitre = db.Column(db.Integer, primary_key = True)
-    civilite_maitre = db.Column(db.String(50), nullable = False)
-    nom_maitre = db.Column(db.String(100), nullable = False)
-    prenom_maitre = db.Column(db.String(100), nullable = False)
-    tel_maitre = db.Column(db.String(15), nullable = True)
-    email_maitre = db.Column(db.String(200), nullable = False)
+    id_maitre = db.Column(db.Integer, primary_key=True)
+    civilite_maitre = db.Column(db.String(50), nullable=False)
+    nom_maitre = db.Column(db.String(100), nullable=False)
+    prenom_maitre = db.Column(db.String(100), nullable=False)
+    tel_maitre = db.Column(db.String(15), nullable=True)
+    email_maitre = db.Column(db.String(200), nullable=False)
 
     # 1,1
-    id_entreprise = db.Column(db.Integer, db.ForeignKey("entreprise.id_entreprise"), nullable = False)
-    entreprise = db.relationship("Entreprise", backref = db.backref("maitre_stage", lazy = "joined", uselist = False))
+    id_entreprise = db.Column(db.Integer,
+                              db.ForeignKey("entreprise.id_entreprise"),
+                              nullable=False)
+    entreprise = db.relationship("Entreprise",
+                                 backref=db.backref("maitre_stage",
+                                                    lazy="joined",
+                                                    uselist=False))
 
-    def __init__(self, civilite_maitre, nom_maitre, prenom_maitre, email_maitre, id_entreprise, tel_maitre = None):
+    def __init__(self,
+                 civilite_maitre,
+                 nom_maitre,
+                 prenom_maitre,
+                 email_maitre,
+                 id_entreprise,
+                 tel_maitre=None):
         self.civilite_maitre = civilite_maitre
         self.nom_maitre = nom_maitre
         self.prenom_maitre = prenom_maitre
@@ -121,15 +178,18 @@ class MaitreStage(db.Model):
 
 
 class Soutenance(db.Model):
-    id_soutenance = db.Column(db.Integer, primary_key = True)
-    salle = db.Column(db.Integer, nullable = False)
-    nom_bat = db.Column(db.String(50), nullable = False)
-    dateS = db.Column(db.Date, nullable = False)
-    h_debut = db.Column(db.String(5), nullable = False)
-    h_fin = db.Column(db.String(5), nullable = False)
+    id_soutenance = db.Column(db.Integer, primary_key=True)
+    salle = db.Column(db.Integer, nullable=False)
+    nom_bat = db.Column(db.String(50), nullable=False)
+    dateS = db.Column(db.Date, nullable=False)
+    h_debut = db.Column(db.String(5), nullable=False)
+    h_fin = db.Column(db.String(5), nullable=False)
 
     # 1,1
-    id_stage = db.Column(db.Integer, db.ForeignKey("stage.id_stage"), unique = True, nullable = False)
+    id_stage = db.Column(db.Integer,
+                         db.ForeignKey("stage.id_stage"),
+                         unique=True,
+                         nullable=False)
 
     def __init__(self, salle, nom_bat, dateS, h_debut, h_fin, id_stage):
         self.salle = salle
@@ -142,36 +202,45 @@ class Soutenance(db.Model):
     def __repr__(self):
         return f"<La soutenance a lieu le {self.date} à {self.h_debut} au batîment {self.nom_bat} {self.salle}>"
 
-    
+
 class Etudiant(db.Model):
-    id_etudiant = db.Column(db.Integer, primary_key = True)
-    nom_etudiant = db.Column(db.String(100), nullable = False)
-    prenom_etudiant = db.Column(db.String(100), nullable = False)
-    civilite_etudiant=db.Column(db.String(10))
-    date_naissance = db.Column(db.Date, nullable = False)
+    id_etudiant = db.Column(db.Integer, primary_key=True)
+    nom_etudiant = db.Column(db.String(100), nullable=False)
+    prenom_etudiant = db.Column(db.String(100), nullable=False)
+    civilite_etudiant = db.Column(db.String(10))
+    date_naissance = db.Column(db.Date, nullable=False)
     telephone_etudiant = db.Column(db.String(15))
     email_etudiant = db.Column(db.String(200))
 
-    def __init__(self, nom_etudiant, prenom_etudiant, civilite_etudiant, date_naissance, telephone_etudiant = None, email_etudiant =None):
+    def __init__(self,
+                 nom_etudiant,
+                 prenom_etudiant,
+                 civilite_etudiant,
+                 date_naissance,
+                 telephone_etudiant=None,
+                 email_etudiant=None):
         self.nom_etudiant = nom_etudiant
         self.prenom_etudiant = prenom_etudiant
         self.civilite_etudiant = civilite_etudiant
         self.date_naissance = date_naissance
         self.telephone_etudiant = telephone_etudiant
         self.email_etudiant = email_etudiant
-    
+
     def __repr__(self):
         return f"<Etudiant {self.nom_etudiant} {self.prenom_etudiant}>"
 
 
 class Promo(db.Model):
-    nom_promo = db.Column(db.String(100), primary_key = True)
-    annee_promo = db.Column(db.Integer, primary_key = True)
-    formation_promo = db.Column(db.String(100), nullable = False)
+    nom_promo = db.Column(db.String(100), primary_key=True)
+    annee_promo = db.Column(db.Integer, primary_key=True)
+    formation_promo = db.Column(db.String(100), nullable=False)
 
     # 0,1
-    id_enseignant = db.Column(db.Integer, db.ForeignKey("enseignant.id_enseignant"), nullable = True)
-    enseignant = db.relationship("Enseignant", backref = db.backref("promos", lazy = "select"))
+    id_enseignant = db.Column(db.Integer,
+                              db.ForeignKey("enseignant.id_enseignant"),
+                              nullable=True)
+    enseignant = db.relationship("Enseignant",
+                                 backref=db.backref("promos", lazy="select"))
 
     def __init__(self, nom_promo, annee_promo, formation_promo):
         self.nom_promo = nom_promo
@@ -183,12 +252,16 @@ class Promo(db.Model):
 
 
 class Appartenir(db.Model):
-    id_etudiant = db.Column(db.Integer, db.ForeignKey("etudiant.id_etudiant"), primary_key = True)
-    etudiant = db.relationship("Etudiant", backref = db.backref(""), lazy = "dynamic")
-    
-    nom_promo = db.Column(db.String(100), primary_key = True)
-    annee_promo = db.Column(db.Integer, primary_key = True)
-    
+    id_etudiant = db.Column(db.Integer,
+                            db.ForeignKey("etudiant.id_etudiant"),
+                            primary_key=True)
+    etudiant = db.relationship("Etudiant",
+                               backref=db.backref(""),
+                               lazy="dynamic")
+
+    nom_promo = db.Column(db.String(100), primary_key=True)
+    annee_promo = db.Column(db.Integer, primary_key=True)
+
     regime_etudiant = db.Column(db.String(100))
 
     def __init__(self, id_etudiant, nom_promo, annee_promo, regime_etudiant):
@@ -202,7 +275,7 @@ class Appartenir(db.Model):
 
 
 class Enseignant(db.Model):
-    id_enseignant = db.Column(db.Integer, primary_key = True)
+    id_enseignant = db.Column(db.Integer, primary_key=True)
     nom_enseignant = db.Column(db.String(100))
     prenom_enseignant = db.Column(db.String(100))
     civilite_enseignant = db.Column(db.String(10))
@@ -219,15 +292,21 @@ class Enseignant(db.Model):
 
 
 class Jury(db.Model):
-    date_jury = db.Column(db.Date, primary_key = True)
+    date_jury = db.Column(db.Date, primary_key=True)
     h_jury = db.Column(db.String(5))
     duree_jury = db.Column(db.Integer)  # durée en minutes
-    
-    # 0,1
-    id_soutenance = db.Column(db.Integer, db.ForeignKey("soutenance.id_soutenance"), unique = True, nullable = True)
-    soutenance = db.relationship("Soutenance", backref = db.backref("jury", lazy = "select", uselist = False))
 
-    def __init__(self, date_jury, heure_jury, duree_jury, id_soutenance = None):
+    # 0,1
+    id_soutenance = db.Column(db.Integer,
+                              db.ForeignKey("soutenance.id_soutenance"),
+                              unique=True,
+                              nullable=True)
+    soutenance = db.relationship("Soutenance",
+                                 backref=db.backref("jury",
+                                                    lazy="select",
+                                                    uselist=False))
+
+    def __init__(self, date_jury, heure_jury, duree_jury, id_soutenance=None):
         self.date_jury = date_jury
         self.h_jury = heure_jury
         self.duree_jury = duree_jury
@@ -239,12 +318,20 @@ class Jury(db.Model):
 
 class Composer(db.Model):
     # 0,N
-    id_enseignant = db.Column(db.Integer, db.ForeignKey("enseignant.id_enseignant"), primary_key=True)
-    enseignant = db.relationship("Enseignant", backref = db.backref("compositions", lazy = "dynamic"))
-    
+    id_enseignant = db.Column(db.Integer,
+                              db.ForeignKey("enseignant.id_enseignant"),
+                              primary_key=True)
+    enseignant = db.relationship("Enseignant",
+                                 backref=db.backref("compositions",
+                                                    lazy="dynamic"))
+
     # 0,N
-    id_soutenance = db.Column(db.Integer, db.ForeignKey("soutenance.id_soutenance"), primary_key=True)
-    soutenance = db.relationship("Soutenance", backref = db.backref("compositions", lazy = "dynamic"))
+    id_soutenance = db.Column(db.Integer,
+                              db.ForeignKey("soutenance.id_soutenance"),
+                              primary_key=True)
+    soutenance = db.relationship("Soutenance",
+                                 backref=db.backref("compositions",
+                                                    lazy="dynamic"))
 
     def __init__(self, id_enseignant, id_soutenance):
         self.id_enseignant = id_enseignant
@@ -254,12 +341,18 @@ class Composer(db.Model):
 class Tutorer(db.Model):
     annee = db.Column(db.Integer)
     # 0,N
-    id_enseignant = db.Column(db.Integer, db.ForeignKey("enseignant.id_enseignant"), primary_key = True)
-    enseignant = db.relationship("Enseignant", backref=db.backref("tutorats", lazy = "dynamic"))
+    id_enseignant = db.Column(db.Integer,
+                              db.ForeignKey("enseignant.id_enseignant"),
+                              primary_key=True)
+    enseignant = db.relationship("Enseignant",
+                                 backref=db.backref("tutorats", lazy="dynamic"))
 
     # 0,N
-    id_etudiant = db.Column(db.Integer, db.ForeignKey("etudiant.id_etudiant"), primary_key = True)
-    etudiant = db.relationship("Etudiant", backref=db.backref("tutorats", lazy = "dynamic"))
+    id_etudiant = db.Column(db.Integer,
+                            db.ForeignKey("etudiant.id_etudiant"),
+                            primary_key=True)
+    etudiant = db.relationship("Etudiant",
+                               backref=db.backref("tutorats", lazy="dynamic"))
 
     def __init__(self, id_enseignant, id_etudiant, annee):
         self.id_enseignant = id_enseignant
