36d5e8f72bf15d1b9205b5699e4dcd44c22310a5
PernelleBlandin <pernelleblandin@gmail.com>
2025-11-05 15:27:15 +0100

    Complétion des URL de l'ensemble des pages des maquettes dans views.py et ajout de templates

--- appSoutenance/views.py
+++ appSoutenance/views.py
@@ -11,9 +11,17 @@ def index():
 def accueil_enseignant():
     return render_template("enseignant/accueil_enseignant.html", title="Accueil")
 
+@app.route('/enseignant/planning/')
+def planning_enseignant():
+    return render_template("enseignant/planning_enseignant.html", title="Planning enseignant")
+
 @app.route('/enseignant/liste+etu/')
 def liste_etu_enseignant():
     return render_template("enseignant/lst_etudiants_enseignant.html", title="Liste des étudiants")
+
+@app.route('/enseignant/liste+etu/etudiant/')
+def detail_etudiant_ens():
+    return render_template("admin/detail_etudiant__ens.html", title="Detail de l'etudiant")
 
 
 #Pour page étudiants
@@ -21,12 +29,47 @@ def liste_etu_enseignant():
 def accueil_etudiant():
     return render_template("etudiant/accueil_etu.html", title="Accueil")
 
+@app.route('/etudiant/demarches/')
+def demarches_etudiant():
+    return render_template("etudiant/demarches.html", title="Mes démarches")
+
+@app.route('/etudiant/stage/')
+def accueil_etudiant():
+    return render_template("etudiant/info_stage_valide.html", title="Mon stage")
+
+@app.route('/etudiant/demarches/new/')
+def nouvelle_demarches_etudiant():
+    return render_template("etudiant/nouvelle_demarches.html", title="Nouvelle démarche")
+
+@app.route('/etudiant/demarches/resume/')
+def resume_demarches_etudiant():
+    return render_template("etudiant/resume_demarches.html", title="Résumé de la démarche")
 
 
 #Pour page administrateur
 @app.route('/admin/')
 def accueil_admin():
     return render_template("admin/accueil_admin.html", title="Accueil")
+
+@app.route('/admin/planning/')
+def planning_admin():
+    return render_template("admin/planning_admin.html", title="Plannng")
+
+@app.route('/admin/liste+enseignants/')
+def liste_ens_admin():
+    return render_template("admin/lst_enseignants_admin.html", title="Liste enseignants")
+
+@app.route('/admin/liste+etudiants/')
+def liste_etu_admin():
+    return render_template("admin/lst_etudiants_admin.html", title="Liste etudiants")
+
+@app.route('/admin/enseignant/')
+def detail_enseignant():
+    return render_template("admin/detail_enseignant.html", title="Detail de l'enseignant")
+
+@app.route('/admin/etudiant/')
+def detail_etudiant_admin():
+    return render_template("admin/detail_etudiant_admin.html", title="Detail de l'etudiant")
 
 
 
