4545f601da001c4f165cfd1c37e95caaaab4b6bb
PernelleBlandin <pernelleblandin@gmail.com>
2025-11-06 11:06:07 +0100

    fin page resume_demarche et début pages nouvelle_demarche. Ajout image de chargement dans static/

--- appSoutenance/views.py
+++ appSoutenance/views.py
@@ -37,12 +37,20 @@ def demarches_etudiant():
 def info_stage():
     return render_template("etudiant/info_stage_valide.html", title="Mon stage")
 
-@app.route('/etudiant/demarches/new/')
-def nouvelle_demarches_etudiant():
-    return render_template("etudiant/nouvelle_demarches.html", title="Nouvelle démarche")
+@app.route('/etudiant/demarches/new1/')
+def nouvelle_demarche1():
+    return render_template("etudiant/nouvelle_demarche1.html", title="Nouvelle démarche")
+
+@app.route('/etudiant/demarches/new2/')
+def nouvelle_demarche2():
+    return render_template("etudiant/nouvelle_demarche2.html", title="Nouvelle démarche")
+
+@app.route('/etudiant/demarches/new3/')
+def nouvelle_demarche3():
+    return render_template("etudiant/nouvelle_demarche3.html", title="Nouvelle démarche")
 
 @app.route('/etudiant/demarches/resume/')
-def resume_demarches_etudiant():
+def resume_demarche_etudiant():
     return render_template("etudiant/resume_demarche.html", title="Résumé de la démarche")
 
 
